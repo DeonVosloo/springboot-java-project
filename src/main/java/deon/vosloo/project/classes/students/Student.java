c4f54bfe6686657054b6a89741d331ef819f10f8
DeonVosloo <deonvosloo3366@gmail.com>
2022-08-29 15:53:19 +0200

    Changes made on Student,java and app.properties file

--- src/main/java/deon/vosloo/project/classes/students/Student.java
+++ src/main/java/deon/vosloo/project/classes/students/Student.java
@@ -2,14 +2,31 @@ package deon.vosloo.project.classes.students;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.sql.Date;
 
-@Entity ()
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
 public class Student 
 {   
-    private @Id int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
 	private String fullName;
+
+    @Column
 	private String address;
+
+    @Column
 	private String email;
+
+    @Column
     private String password;
 
     public int getStudentID() {
