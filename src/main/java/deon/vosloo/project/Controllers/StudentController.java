ff8099c5b9ee7153e628c735435ab034db8df52c
DeonVosloo <deonvosloo3366@gmail.com>
2022-09-01 15:42:20 +0200

    Added update and delete of CRUD to StudentController.java

--- src/main/java/deon/vosloo/project/Controllers/StudentController.java
+++ src/main/java/deon/vosloo/project/Controllers/StudentController.java
@@ -3,8 +3,11 @@ package deon.vosloo.project.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +38,28 @@ public class StudentController
     public String saveStudent(@RequestBody Student student) {
         studentRepository.save(student);
         return "Student saved to database";
+    }
+
+    @PutMapping("/update-student/{id}")
+    public String updateStudent(@PathVariable Long id, @RequestBody Student student) 
+    {
+        Student updateStudent = studentRepository.findById(id).get();
+
+        updateStudent.setFullName(student.getFullName());
+        updateStudent.setAddress(student.getAddress());
+        updateStudent.setEmail(student.getEmail());
+        updateStudent.setPassword(student.getPassword());
+
+        studentRepository.save(updateStudent);
+
+        return "updated Student";
+    }
+
+    @DeleteMapping("/delete-student/{id}")
+    public String deleteStudent(@PathVariable Long id) 
+    {
+        Student deleteStudent = studentRepository.findById(id).get();
+        studentRepository.delete(deleteStudent);
+        return "Deleted student: " + id + " " + deleteStudent.getFullName();
     }
 }
