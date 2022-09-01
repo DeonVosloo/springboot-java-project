bb288d494c4335bdbc6335310afade27cb123a6b
DeonVosloo <deonvosloo3366@gmail.com>
2022-09-01 09:28:29 +0200

    Fixed Error in studentController.java

--- src/main/java/deon/vosloo/project/Controllers/StudentController.java
+++ src/main/java/deon/vosloo/project/Controllers/StudentController.java
@@ -1,14 +1,35 @@
 package deon.vosloo.project.Controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import deon.vosloo.project.Modals.Student;
+import deon.vosloo.project.repository.StudentRepository;
+
 @RestController
-public class StudentController {
-    
-    @GetMapping()
+public class StudentController 
+{
+    private StudentRepository studentRepository;
+
+    @GetMapping("/")
     public String getPage() 
     {
         return "Welcome";
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() 
+    {
+        return studentRepository.findAll();
+    }
+
+    @PostMapping("/save-student")
+    public String saveStudent(@RequestBody Student student) {
+        studentRepository.save(student);
+        return "Student saved to database";
     }
 }
