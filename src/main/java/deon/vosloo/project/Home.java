70be454fc1e6e223ed194c0284492bf45ca6db3b
DeonVosloo <deonvosloo3366@gmail.com>
2022-09-01 17:44:59 +0200

    Added More comments regarding CORS implementation

--- src/main/java/deon/vosloo/project/Home.java
+++ src/main/java/deon/vosloo/project/Home.java
@@ -12,8 +12,10 @@ public class Home {
         SpringApplication.run(Home.class);
     }
 
-    //the code below is to enable CORS which allows strict-origin-when-cross-origin to work (spring API data can be used in REACT)
+    //the code below is to enable CORS which allows cross origin to work (spring API data can be used in REACT)
     //Each @Mapping annotation must be mapped below if I want there to be no strict-origin-when-cross-origin error in REACT when trying to read that API data in REACT
+    // CORS fixes the strict-origin-when-cross-origin error that happens on the client side. CORS must be implemented serverside
+    //Below is an example of an global CORS implementation.
     @Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
