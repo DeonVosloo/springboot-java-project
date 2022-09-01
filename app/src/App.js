9a22c9ea652d9f41b54b9a607852a2eafdee3366
DeonVosloo <deonvosloo3366@gmail.com>
2022-09-01 16:32:53 +0200

    Cleaned up app.js

--- app/src/App.js
+++ app/src/App.js
@@ -5,23 +5,20 @@ import {useState, useEffect ,React} from 'react';
 const App = () => {
 
   const [students, setStudents] = useState([]);
-  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-
-    fetch('/api/students')
-      .then(response => response.json())
-      .then(data => {
+  let getStudentsData = () => {
+    fetch("http://localhost:8081/api/students")
+      .then((response) => response.json())
+      .then((data) => {      
         setStudents(data);
-        setLoading(false);
-      })
+      });
+  };
+
+  useEffect(() => 
+  {
+    getStudentsData();
+    console.log("student data loaded");
   }, []);
-
-  // if (loading) {
-  //   return <p>Loading...</p>;
-  // }
-
 
   return (
     <div className="App">
