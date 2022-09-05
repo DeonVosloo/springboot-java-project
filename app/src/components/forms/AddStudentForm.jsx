82fa532ce4ec9cb29e2d275ec0bf1d4cd3e818f7
DeonVosloo <deonvosloo3366@gmail.com>
2022-09-05 10:47:16 +0200

    Improved add Student Form readability

--- app/src/components/forms/AddStudentForm.jsx
+++ app/src/components/forms/AddStudentForm.jsx
@@ -6,6 +6,9 @@ import Typography from '@mui/material/Typography';
 import FormControl, { useFormControl } from '@mui/material/FormControl';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import FormHelperText from '@mui/material/FormHelperText';
+import Button from '@mui/material/Button'
+
+
 
 function MyFormHelperText(props) {
     const { focused } = useFormControl() || {};
@@ -22,30 +25,80 @@ function MyFormHelperText(props) {
   }
 
 function AddStudentForm() {
+
+  let student = {
+    name: "",
+    address: "",
+    email: "",
+    password: "",
+    courseName: ""
+  }
   return (
     <div>
         <Appheader/>
 
-        <Grid container spacing={2} sx={{display: "flex", justifyContent: "center"}}>
+        <Grid container spacing={2} sx={{display: "flex", justifyContent: "center", paddingBottom: "3%"}}>
             <Grid item xs={11} md={10} lg={9}>
                 <Box sx={{ flexGrow: 1, border: 1, borderRadius: 2.5, bgcolor: "whitesmoke"}}>
                     <Grid container spacing={2} sx={{display: "flex", justifyContent: "center"}}>
                         <Grid item xs={11.3}>
-                            <Typography variant="h2" color="initial">Add Student</Typography>
+                            <Typography variant="h2" color="initial" sx={{fontFamily: `Ubuntu, sans-serif`, marginTop: "2%"}}>Add Student</Typography>
+                            <Typography variant="h4" color="initial" sx={{fontFamily: `Ubuntu, sans-serif`, marginTop: "1%"}}>Please enter the student details</Typography>
                         </Grid>
                         <Grid item xs={11.3}>
                             <Box component="form" noValidate autoComplete="off">
-                                <FormControl sx={{ width: 800, maxWidth: '100%' }}>
-                                    <OutlinedInput placeholder="Please enter the students full name" variant="standard" />
-                                    <MyFormHelperText hintText="Enter the students full name" focusedText="Currently Typing the students name" />
+                                <FormControl sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}>
+                                  <Typography variant="p" color="initial" sx={{
+                                    display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%", fontFamily: `Ubuntu, sans-serif`
+                                    }}>Full Name</Typography>
+                                    <OutlinedInput placeholder="Full Name" variant="standard" />
+                                    <MyFormHelperText hintText="" focusedText="Currently Typing the students name" />
                                 </FormControl>
                             </Box>
                         </Grid>
                         <Grid item xs={11.3}>
-
+                          <Box component="form" noValidate autoComplete="off">
+                            <FormControl sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}>
+                                <Typography variant="p" color="initial" sx={{display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%"}}>Address</Typography>
+                                <OutlinedInput placeholder="Address" variant="standard" />
+                                <MyFormHelperText hintText="" focusedText="Currently Typing the students Address" />
+                            </FormControl>
+                          </Box>
                         </Grid>
                         <Grid item xs={11.3}>
+                          <Box component="form" noValidate autoComplete="off">
+                            <FormControl sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}>
+                                <Typography variant="p" color="initial" sx={{display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%"}}>Email</Typography>
+                                <OutlinedInput placeholder="Email" variant="standard" />
+                                <MyFormHelperText hintText="" focusedText="Currently Typing the students Email" />
+                            </FormControl>
+                          </Box>   
+                        </Grid>
+                        <Grid item xs={11.3}>
+                          <Box component="form" noValidate autoComplete="off">
+                            <FormControl sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}>
+                                <Typography variant="p" color="initial" sx={{display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%"}}>Password</Typography>
+                                <OutlinedInput placeholder="Password" variant="standard" />
+                                <MyFormHelperText hintText="" focusedText="Currently Typing the students Password" />
+                            </FormControl>
+                          </Box>   
+                        </Grid>
+                        <Grid item xs={11.3}>
+                          <Box component="form" noValidate autoComplete="off">
+                            <FormControl sx={{ width: 800, maxWidth: '100%', marginBottom: "1%", fontFamily: `Ubuntu, sans-serif` }}>
+                                <Typography variant="p" color="initial" sx={{display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%"}}>Course name</Typography>
+                                <OutlinedInput placeholder="Course name" variant="standard" />
+                                <MyFormHelperText hintText="" focusedText="Currently Typing the students Course Name" />
 
+                                <Button variant="contained" sx={{
+                                  maxWidth: "100%", width: "70%", height: 41, bgcolor: "green", marginBottom: "3%", marginTop: "4%", marginLeft: "15%",
+                                  display: "flex", justifyContent: "center"
+                                  }} >
+                                  <Typography variant="p" color="initial" sx={{fontFamily: `Ubuntu, sans-serif`}}>Add Student</Typography>
+                                </Button>
+
+                            </FormControl>                     
+                          </Box>   
                         </Grid>
                     </Grid>
                 </Box>
