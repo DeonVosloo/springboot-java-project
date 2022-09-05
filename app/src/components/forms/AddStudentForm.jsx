333de8166040a0ea213418dbc3b077cddd18d670
DeonVosloo <deonvosloo3366@gmail.com>
2022-09-05 16:32:56 +0200

    Add Student Form is now properly  adding a new student to database

--- app/src/components/forms/AddStudentForm.jsx
+++ app/src/components/forms/AddStudentForm.jsx
@@ -4,10 +4,10 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import FormControl, { useFormControl } from '@mui/material/FormControl';
-import OutlinedInput from '@mui/material/OutlinedInput';
 import FormHelperText from '@mui/material/FormHelperText';
 import Button from '@mui/material/Button'
-
+import { useState } from 'react';
+import TextField from '@mui/material/TextField'
 
 
 function MyFormHelperText(props) {
@@ -26,13 +26,56 @@ function MyFormHelperText(props) {
 
 function AddStudentForm() {
 
+  const [students, setStudents] = useState([]);
+
+  let addStudent = async () => {
+    console.log(student.name + " " + student.address + " " + student.email + " " + student.password + " " + student.course);
+    await fetch(`/api/save-student`, {
+      method: 'POST',
+      body: JSON.stringify(student),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "X-Requested-With"
+      },
+    }).then(response => response.json(),
+    console.log("added student: " + student)
+    )}
+
   let student = {
-    name: "",
+    fullName: "",
     address: "",
     email: "",
     password: "",
-    courseName: ""
-  }
+    courseName: "",
+  };
+
+  let handleNameChange = (e) =>
+  {
+    student.fullName = e.target.value;
+  };
+
+  let handleAddressChange = (e) =>
+  {
+    student.address = e.target.value;
+  };
+
+  let handleEmailChange = (e) =>
+  {
+    student.email = e.target.value;
+  };
+  let handlePasswordChange = (e) =>
+  {
+    student.password = e.target.value;
+  };
+  let handleCourseChange = (e) =>
+  {
+    student.courseName = e.target.value;
+  };
+
+
+
   return (
     <div>
         <Appheader/>
@@ -46,54 +89,35 @@ function AddStudentForm() {
                             <Typography variant="h4" color="initial" sx={{fontFamily: `Ubuntu, sans-serif`, marginTop: "1%"}}>Please enter the student details</Typography>
                         </Grid>
                         <Grid item xs={11.3}>
-                            <Box component="form" noValidate autoComplete="off">
-                                <FormControl sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}>
-                                  <Typography variant="p" color="initial" sx={{
-                                    display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%", fontFamily: `Ubuntu, sans-serif`
-                                    }}>Full Name</Typography>
-                                    <OutlinedInput placeholder="Full Name" variant="standard" />
-                                    <MyFormHelperText hintText="" focusedText="Currently Typing the students name" />
-                                </FormControl>
-                            </Box>
-                        </Grid>
-                        <Grid item xs={11.3}>
-                          <Box component="form" noValidate autoComplete="off">
-                            <FormControl sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}>
-                                <Typography variant="p" color="initial" sx={{display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%"}}>Address</Typography>
-                                <OutlinedInput placeholder="Address" variant="standard" />
-                                <MyFormHelperText hintText="" focusedText="Currently Typing the students Address" />
-                            </FormControl>
+                        <Box>                     
+                              <TextField id="" label="Full Name" onChange={handleNameChange} sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}/>
                           </Box>
                         </Grid>
                         <Grid item xs={11.3}>
-                          <Box component="form" noValidate autoComplete="off">
-                            <FormControl sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}>
-                                <Typography variant="p" color="initial" sx={{display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%"}}>Email</Typography>
-                                <OutlinedInput placeholder="Email" variant="standard" />
-                                <MyFormHelperText hintText="" focusedText="Currently Typing the students Email" />
-                            </FormControl>
-                          </Box>   
+                          <Box>                     
+                              <TextField id="" label="Address" onChange={handleAddressChange} sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}/>
+                          </Box>
                         </Grid>
                         <Grid item xs={11.3}>
-                          <Box component="form" noValidate autoComplete="off">
-                            <FormControl sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}>
-                                <Typography variant="p" color="initial" sx={{display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%"}}>Password</Typography>
-                                <OutlinedInput placeholder="Password" variant="standard" />
-                                <MyFormHelperText hintText="" focusedText="Currently Typing the students Password" />
-                            </FormControl>
-                          </Box>   
+                        <Box>                     
+                              <TextField id="" label="Email" onChange={handleEmailChange} sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}/>
+                          </Box>
+                        </Grid>
+                        <Grid item xs={11.3}>
+                          <Box>                     
+                              <TextField id="" label="Password" type={"password"} onChange={handlePasswordChange} sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}/>
+                          </Box>
+ 
                         </Grid>
                         <Grid item xs={11.3}>
                           <Box component="form" noValidate autoComplete="off">
                             <FormControl sx={{ width: 800, maxWidth: '100%', marginBottom: "1%", fontFamily: `Ubuntu, sans-serif` }}>
-                                <Typography variant="p" color="initial" sx={{display: "flex", justifyContent: "flex-start", paddingLeft: "1%", paddingBottom: "1%"}}>Course name</Typography>
-                                <OutlinedInput placeholder="Course name" variant="standard" />
-                                <MyFormHelperText hintText="" focusedText="Currently Typing the students Course Name" />
+                            <TextField id="" label="Course Name" onChange={handleCourseChange} sx={{ width: 800, maxWidth: '100%', fontFamily: `Ubuntu, sans-serif` }}/>
 
                                 <Button variant="contained" sx={{
                                   maxWidth: "100%", width: "70%", height: 41, bgcolor: "green", marginBottom: "3%", marginTop: "4%", marginLeft: "15%",
                                   display: "flex", justifyContent: "center"
-                                  }} >
+                                  }} onClick={(addStudent)}>
                                   <Typography variant="p" color="initial" sx={{fontFamily: `Ubuntu, sans-serif`}}>Add Student</Typography>
                                 </Button>
 
